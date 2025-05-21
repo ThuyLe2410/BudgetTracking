69ba37714e17bb6cccefab1777a56b9c183b0dbb
Thuy Le <ngocthuyle19@gmail.com>
2025-05-21 10:54:18 -0400

    hide uncategorized

--- Frontend/src/components/AddBudgetModal.tsx
+++ Frontend/src/components/AddBudgetModal.tsx
@@ -1,24 +1,30 @@
-
-
 import { useForm } from "react-hook-form";
 import { useBudgets } from "../contexts/BudgetContext";
-import type { AddBudgetModalProps, budgetCategoryProps, BudgetFormInput, budgetProps } from "../types";
+import type {
+  AddBudgetModalProps,
+  budgetCategoryProps,
+  BudgetFormInput,
+  budgetProps,
+} from "../types";
 
 export default function AddBudgetModal({
   show,
   handleClose,
 }: AddBudgetModalProps) {
   const { register, handleSubmit, reset } = useForm<BudgetFormInput>();
-  const {budgetCategory, addBudget} = useBudgets() as {
-    budgetCategory: budgetCategoryProps[],
+  const { budgetCategory, addBudget } = useBudgets() as {
+    budgetCategory: budgetCategoryProps[];
     addBudget: (data: budgetProps) => void;
-  }
+  };
 
   const onSubmit = (data: BudgetFormInput) => {
-    
     const category = budgetCategory.find((b) => b.id === Number(data.id));
     if (!category) return;
-    addBudget({ id: Number(data.id), name: category.name, max: Number(data.maxSpending) });
+    addBudget({
+      id: Number(data.id),
+      name: category.name,
+      max: Number(data.maxSpending),
+    });
     reset();
     handleClose();
   };
@@ -35,27 +41,28 @@ export default function AddBudgetModal({
           </div>
 
           <p className="mt-3">Name</p>
-          <select className="w-full h-10 border-1 p-1"
-          {...register("id", { required: true })}>
-            {budgetCategory.map((b: budgetCategoryProps) => (
-              <option key={b.id} value={b.id}>{b.name}</option>
-            ))}
+          <select
+            className="w-full h-10 border-1 p-1"
+            {...register("id", { required: true })}>
+            {budgetCategory
+              .filter((b) => b.id !== 1)
+              .map((b: budgetCategoryProps) => (
+                <option key={b.id} value={b.id}>
+                  {b.name}
+                </option>
+              ))}
           </select>
-       
+
           <p className="mt-3">Maximum spending</p>
           <input
             className="w-full h-10 border-1 p-1"
             {...register("maxSpending", { required: true })}
           />
           <div className="flex justify-end mt-3">
-          <button
-            className="bg-green-600 rounded"
-            type="submit">
-            Add
-          </button>
+            <button className="bg-green-600 rounded" type="submit">
+              Add
+            </button>
           </div>
-
-
         </form>
       </div>
     </>
