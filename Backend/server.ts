628ac85291f78b21d309749b813375e56e7449f3
Thuy Le <ngocthuyle19@gmail.com>
2025-05-22 09:39:12 -0400

    delete Budget

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,82 +1,119 @@
-import {budgetCategory, budgets, expenseCategory, expenses} from "./src/db/schema"
-import {db} from "./src/db/index";
-import {sql} from "drizzle-orm";
-import {eq} from "drizzle-orm"
+import {
+  budgetCategory,
+  budgets,
+  expenseCategory,
+  expenses,
+} from "./src/db/schema";
+import { db } from "./src/db/index";
+import { sql } from "drizzle-orm";
+import { eq } from "drizzle-orm";
 
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(cors({
-    origin: "http://localhost:5174",
-    methods: ["GET", "POST", "DELETE", "PUT"]
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  })
+);
 
 // fetch budgetCategory
-app.get("/budgetCategory", async(req, res) => {
-    const queryResult = await db.select().from(budgetCategory);
-    res.send({data:queryResult})
-})
+app.get("/budgetCategory", async (req, res) => {
+  const queryResult = await db.select().from(budgetCategory);
+  res.send({ data: queryResult });
+});
 
 // fetch budgets Data
-app.get("/budgets", async(req, res) => {
-    const queryResult = await db.select().from(budgets);
-    res.send({data: queryResult})
-})
+app.get("/budgets", async (req, res) => {
+  const queryResult = await db.select().from(budgets);
+  res.send({ data: queryResult });
+});
 
 // Add new budget
-app.post("/budgets", async(req, res) => {
-    const insertData = {
-        name: req.body.name,
-        max: req.body.max,
-        id: req.body.id
-    }
-    console.log('insertData', req.body)
-    await db.insert(budgets).values(insertData);
-    console.log("Add new budget")
-})
+app.post("/budgets", async (req, res) => {
+  const insertData = {
+    name: req.body.name,
+    max: req.body.max,
+    id: req.body.id,
+  };
+  console.log("insertData", req.body);
+  await db.insert(budgets).values(insertData);
+  console.log("Add new budget");
+});
 
 // Delete budget
 app.delete("/budgets/:id", async (req, res) => {
-    const id = parseInt(req.params.id, 10);
-    const totalAmount = await db.select({sum: sql<number>`SUM(${expenses.amount})`}).from(expenses).where(eq(expenses.budgetId, id));
-    const amount = totalAmount[0]?.sum ?? 0
-    console.log("amount", amount);
-    const uncategorizedBudget = await db.select().from(budgets).where(eq(budgets.id, 1));
-    if (uncategorizedBudget.length>0) {
-        const uncategorized = uncategorizedBudget[0];
-        const newMax = Number(uncategorized.max ?? 0) + amount
-    }
-    await db.update(budgets).set({id: 1, name:"Uncategorized", max :amount}).where(eq(budgets.id, id));
-    await db.update(expenses).set({budgetId:1}).where(eq(expenses.budgetId, id));
-})
+  const id = parseInt(req.params.id, 10);
+  const totalAmount = await db
+    .select({ sum: sql<number>`SUM(${expenses.amount})` })
+    .from(expenses)
+    .where(eq(expenses.budgetId, id));
+  const amount = totalAmount[0]?.sum ?? 0;
+  console.log("amount", amount);
+  const uncategorizedBudget = await db
+    .select()
+    .from(budgets)
+    .where(eq(budgets.id, 1));
+  let newMax: number;
+  if (uncategorizedBudget.length > 0) {
+    const uncategorized = uncategorizedBudget[0];
+    newMax = Number(uncategorized.max ?? 0) + amount;
+    console.log("newMax", newMax);
+    await db.update(budgets).set({ max: newMax }).where(eq(budgets.id, 1));
+    await db.delete(budgets).where(eq(budgets.id, id));
+  } else {
+    await db
+      .update(budgets)
+      .set({ id: 1, name: "Uncategorized", max: amount })
+      .where(eq(budgets.id, id));
+  }
+  await db
+    .update(expenses)
+    .set({ expenseId:1, name:"Uncategorized", budgetId: 1 })
+    .where(eq(expenses.budgetId, id));
+});
 
 // fetch expense
 app.get("/expenses", async (req, res) => {
-    const queryResult = await db.select().from(expenses)
-    res.send({data: queryResult})
-})
+  const queryResult = await db.select().from(expenses);
+  res.send({ data: queryResult });
+});
 
 // fetch expense Category
 app.get("/expenseCategory", async (req, res) => {
-    const queryResult = await db.select().from(expenseCategory)
-    res.send({data:queryResult});
-    console.log('expenseCategory')
-})
+  const queryResult = await db.select().from(expenseCategory);
+  res.send({ data: queryResult });
+  console.log("expenseCategory");
+});
 
-const run = async () =>  {
-    app.listen(3000, () => {
-        console.log("Server started!")
-    })
-}
+// Add New Expense
+app.post("/expenses", async (req, res) => {
+  console.log("add new Expense", req.body);
+  const name = await db
+    .select({ name: expenseCategory.name })
+    .from(expenseCategory)
+    .where(eq(expenseCategory.id, req.body.expenseId));
 
+  const insertData = {
+    expenseId: req.body.expenseId,
+    name: name[0].name,
+    description: req.body.description,
+    amount: req.body.amount,
+    budgetId: req.body.budgetId,
+  };
+  await db.insert(expenses).values(insertData);
+});
 
+const run = async () => {
+  app.listen(3000, () => {
+    console.log("Server started!");
+  });
+};
 
-run()
-
-
-
+run();
