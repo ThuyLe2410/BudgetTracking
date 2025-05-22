4c103ca0d64abe7dddf6385f4057a27ca6d915c0
Thuy Le <ngocthuyle19@gmail.com>
2025-05-22 10:17:21 -0400

    create new schema

--- Backend/src/db/index.ts
+++ Backend/src/db/index.ts
@@ -1,15 +1,18 @@
 import 'dotenv/config';
 import {drizzle} from "drizzle-orm/node-postgres";
 import {sql} from "drizzle-orm"
-import { budgetCategory, expenseCategory } from './schema';
+import { budgetCategory, expenseCategory, budgets, expenses } from './schema';
 
-const db = drizzle(process.env.DATABASE_URL!)
+export const db = drizzle(process.env.DATABASE_URL!)
 
 async function main() {
-    await db.execute(sql`TRUNCATE TABLE ${budgetCategory}`);
-    await db.execute(sql`TRUNCATE TABLE ${expenseCategory}`);
+    await db.execute(sql`TRUNCATE TABLE ${budgetCategory} RESTART IDENTITY CASCADE`);
+    await db.execute(sql`TRUNCATE TABLE ${expenseCategory} RESTART IDENTITY CASCADE`);
+    // await db.execute(sql`TRUNCATE TABLE ${budgets}`);
+    // await db.execute(sql`TRUNCATE TABLE ${expenses}`)
 
     const budget_category = [
+        {name: "Uncategorized"},
         { name: "Food & Dining" },
         { name: "Utilities" },
         { name: "Auto & Transport" },
@@ -24,49 +27,50 @@ async function main() {
       ];
     
       const expense_category = [
-        { name: "Groceries", budgetId: 1 },
-        { name: "Bars & Coffee", budgetId: 1 },
-        { name: "Restaurant", budgetId: 1 },
-        { name: "Breakfast", budgetId: 1 },
-        { name: "Dinner", budgetId: 1 },
-        { name: "Lunch", budgetId: 1 },
-        { name: "Electricity", budgetId: 2 },
-        { name: "Internet", budgetId: 2 },
-        { name: "Mobile Phone", budgetId: 2 },
-        { name: "Water", budgetId: 2 },
-        { name: "Cable TV", budgetId: 2 },
-        { name: "Gas", budgetId: 2 },
-        { name: "Telephone", budgetId: 2 },
-        { name: "Fuel", budgetId: 3 },
-        { name: "Insurance", budgetId: 3 },
-        { name: "Parking", budgetId: 3 },
-        { name: "Service & Parts", budgetId: 3 },
-        { name: "Taxi", budgetId: 3 },
-        { name: "Pulic Transportation", budgetId: 3 },
-        { name: "Clothes", budgetId: 4 },
-        { name: "Accessories", budgetId: 4 },
-        { name: "Shoes", budgetId: 4 },
-        { name: "Recreation & Leisure", budgetId: 5 },
-        { name: "Movies", budgetId: 5 },
+        {name: "Uncategorized", budgetId: 1},
+        { name: "Groceries", budgetId: 2 },
+        { name: "Bars & Coffee", budgetId: 2 },
+        { name: "Restaurant", budgetId: 2 },
+        { name: "Breakfast", budgetId: 2 },
+        { name: "Dinner", budgetId: 2 },
+        { name: "Lunch", budgetId: 2 },
+        { name: "Electricity", budgetId: 3 },
+        { name: "Internet", budgetId: 3 },
+        { name: "Mobile Phone", budgetId: 3 },
+        { name: "Water", budgetId: 3 },
+        { name: "Cable TV", budgetId: 3 },
+        { name: "Gas", budgetId: 3 },
+        { name: "Telephone", budgetId: 3 },
+        { name: "Fuel", budgetId: 4 },
+        { name: "Insurance", budgetId: 4 },
+        { name: "Parking", budgetId: 4 },
+        { name: "Service & Parts", budgetId: 4 },
+        { name: "Taxi", budgetId: 4 },
+        { name: "Pulic Transportation", budgetId: 4 },
+        { name: "Clothes", budgetId: 5 },
+        { name: "Accessories", budgetId: 5 },
         { name: "Shoes", budgetId: 5 },
-        { name: "Tuition", budgetId: 6 },
-        { name: "Babysitter & Daycare", budgetId: 6 },
-        { name: "Baby Supplies", budgetId: 6 },
-        { name: "Toys", budgetId: 6 },
-        { name: "Gifts", budgetId: 7 },
-        { name: "Donations", budgetId: 7 },
-        { name: "Healthcare", budgetId: 8 },
-        { name: "Health insurance", budgetId: 8 },
-        { name: "Pharmacy", budgetId: 8 },
-        { name: "Sports", budgetId: 8 },
-        { name: "Furnishing", budgetId: 9 },
-        { name: "Home services", budgetId: 9 },
-        { name: "Mortgage & Rent", budgetId: 9 },
-        { name: "Education", budgetId: 10 },
-        { name: "Networking Expenses", budgetId: 10 },
-        { name: "Spa & Massage", budgetId: 10 },
-        { name: "Pet foods", budgetId: 11 },
-        { name: "Pet Stuff", budgetId: 11 },
+        { name: "Recreation & Leisure", budgetId: 6 },
+        { name: "Movies", budgetId: 6 },
+        { name: "Shoes", budgetId: 6 },
+        { name: "Tuition", budgetId: 7 },
+        { name: "Babysitter & Daycare", budgetId: 7 },
+        { name: "Baby Supplies", budgetId: 7 },
+        { name: "Toys", budgetId: 7 },
+        { name: "Gifts", budgetId: 8 },
+        { name: "Donations", budgetId: 8 },
+        { name: "Healthcare", budgetId: 9 },
+        { name: "Health insurance", budgetId: 9 },
+        { name: "Pharmacy", budgetId: 9 },
+        { name: "Sports", budgetId: 9 },
+        { name: "Furnishing", budgetId: 10 },
+        { name: "Home services", budgetId: 10 },
+        { name: "Mortgage & Rent", budgetId: 10 },
+        { name: "Education", budgetId: 11 },
+        { name: "Networking Expenses", budgetId: 11 },
+        { name: "Spa & Massage", budgetId: 11 },
+        { name: "Pet foods", budgetId: 12 },
+        { name: "Pet Stuff", budgetId: 12 },
       ];
 
     await db.insert(budgetCategory).values(budget_category);
@@ -74,7 +78,6 @@ async function main() {
 
     await db.insert(expenseCategory).values(expense_category);
     console.log("New expense_category created");
-
 
 }
 
--- Backend/src/db/schema.ts
+++ Backend/src/db/schema.ts
@@ -1,28 +1,33 @@
-import { pgTable, varchar, serial, integer, numeric } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  varchar,
+  serial,
+  integer,
+  numeric,
+} from "drizzle-orm/pg-core";
 
 export const budgetCategory = pgTable("budget_category", {
   id: serial("id").primaryKey(),
   name: varchar({ length: 255 }).notNull(),
 });
 
-
 export const expenseCategory = pgTable("expense_category", {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 255 }).notNull(),
-    budgetId: integer(),
-  });
-  
-  export const budgets = pgTable("budgets", {
-    id: integer(),
-    name: varchar( { length: 255 }).notNull(),
-    max: numeric( {precision:10, scale:2, mode:'number'}),
-  });
-  
-  export const expenses = pgTable("expenses", {
-      id: integer(),
-      name: varchar( { length: 255 }).notNull(),
-      description: varchar( { length: 255 }).notNull(),  
-      amount: numeric( {precision:10, scale:2, mode:'number'}),
-      budgetId: integer()
-    });
-  
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  budgetId: integer(),
+});
+
+export const budgets = pgTable("budgets", {
+  id: integer().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  max: numeric({ precision: 10, scale: 2, mode: "number" }),
+});
+
+export const expenses = pgTable("expenses", {
+  id: serial("id").primaryKey(),
+  expenseTypeId: integer(),
+  name: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }),
+  amount: numeric({ precision: 10, scale: 2, mode: "number" }),
+  budgetId: integer(),
+});
